7177e5a2ea4c7b82891ff827c056157ea6a60ebd
Michael <michael.sarrazin@webedia-group.com>
2022-04-21 17:36:21 +0200

    correction hover menu + modification burger menu

--- html/inc/nav.php
+++ html/inc/nav.php
@@ -1,18 +1,11 @@
 <nav class="nav navbar">
-            <!-- <a class="nav__item " href="../index.php">Accueil</a>
-            <a class="nav__item" href="../pages/inscription.php">Inscription</a>
-            <a class="nav__item" href="../pages/equipe.php">Equipes</a>
-            <a class="nav__item" href="https://espot.fr/" target="_blank">Espot</a>
-            <a class="nav__item" href="../pages/reglement.php">Règlement</a>
-            <a href="https://discord.gg/URjSmdDa8H" target="_blank"><img src="../dist/assets/img/discord.png" alt="img test" style="width: 30px; display: block; marging-left: 20px;"></a> -->
-
             <ul class="nav-menu">
-                <li class="nav-item"><a href="../index.php">Accueil</a></li>
-                <li class="nav-item"><a href="../pages/inscription.php">Inscription</a></li>
-                <li class="nav-item"><a href="../pages/equipe.php">Equipes</a></li>
-                <li class="nav-item"><a href="https://espot.fr/" target="_blank">Espot</a></li>
-                <li class="nav-item"><a href="../pages/reglement.php">Règlement</a></li>
-                <li class="nav-item"><a href="https://discord.gg/URjSmdDa8H" target="_blank"><img src="../dist/assets/img/discord.png" alt="img test" style="width: 30px; display: block; marging-left: 20px;"></a></li>
+                <li class="nav-item"><a class="nav__item" href="../index.php">Accueil</a></li>
+                <li class="nav-item"><a class="nav__item" href="../pages/inscription.php">Inscription</a></li>
+                <li class="nav-item"><a class="nav__item" href="../pages/equipe.php">Equipes</a></li>
+                <li class="nav-item"><a class="nav__item" href="https://espot.fr/" target="_blank">Espot</a></li>
+                <li class="nav-item"><a class="nav__item" href="../pages/reglement.php">Règlement</a></li>
+                <li class="nav-item"><a class="nav__item" href="https://discord.gg/URjSmdDa8H" target="_blank"><img src="../dist/assets/img/discord.png" alt="img test" style="width: 30px; display: block; marging-left: 20px;"></a></li>
             </ul>
             <div class="burger">
                 <span class="bar"></span>
